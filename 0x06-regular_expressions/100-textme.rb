8afdd49aab9c7ab7d78c026723fd8b70a2aa97a0
MIRARG20 <amiraragab480@gmail.com>
2024-02-28 11:01:30 -0800

    alltasks

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,25 +1,6 @@
 #!/usr/bin/env ruby
 
-# Function to extract information from the log entry
-def extract_info(log_entry)
-    # Regular expression to capture relevant information
-    regex = /\[from:(.+?)\] \[to:(.+?)\] \[flags:(.+?)\]/
-  
-    # Match the regular expression against the log entry
-    match_data = log_entry.match(regex)
-  
-    # If there's a match, extract the sender, receiver, and flags
-    if match_data
-      sender = match_data[1]
-      receiver = match_data[2]
-      flags = match_data[3]
-      "#{sender},#{receiver},#{flags}"
-    else
-      "Invalid log entry format"
-    end
-  end 
-  # Process each log entry and print the result
-  log_entries.each do |log_entry|
-    puts extract_info(log_entry)
-  end
-  
+from = ARGV[0].scan(/from:(.*?)\]/)
+to = ARGV[0].scan(/to:(.*?)\]/)
+flags = ARGV[0].scan(/flags:(.*?)\]/)
+puts [from, to, flags].join(',')
